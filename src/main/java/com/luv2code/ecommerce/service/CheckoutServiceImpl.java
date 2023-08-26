5d3ef438aad3f83d9dafedf1be4233f966876fe3
OTHMANE ERRARHBI <othmane.errarhbi@gmail.com>
2023-08-26 17:28:54 +0100

    build backend architecture : create entities, purchase service and controller

---
+++ src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,58 @@
+package com.luv2code.ecommerce.service;
+
+import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Customer;
+import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+
+    private CustomerRepository customerRepository;
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        final Order order = purchase.getOrder();
+        String orderTrackingNumber = setOrderTrackingNumber(order);
+        processOrderItems(purchase, order);
+        processShippingAndBillingAddress(purchase, order);
+        processCustomer(purchase, order);
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private void processCustomer(Purchase purchase, Order order) {
+        Customer customer = purchase.getCustomer();
+        customer.addOrder(order);
+        customerRepository.save(customer);
+    }
+
+    private String setOrderTrackingNumber(Order order) {
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+        return orderTrackingNumber;
+    }
+
+    private static void processOrderItems(Purchase purchase, Order order) {
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(order::add);
+    }
+
+    private static void processShippingAndBillingAddress(Purchase purchase, Order order) {
+        order.setShippingAddress(purchase.getBillingAddress());
+        order.setBillingAddress(purchase.getShippingAddress());
+    }
+
+    private String generateOrderTrackingNumber() {
+        return UUID.randomUUID().toString();
+    }
+}
